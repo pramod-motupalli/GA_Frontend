2a5dee845d16dde28f777e3cb58b531a9d7bd5f4
Sai Venkata Sirisha Motupalli <motupallisnmsdurgapramod531@gmail.com>
2025-05-19 11:26:05 +0530

    verify email

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password"element={<ResetPassword />}
-        />
+        <Route path="/reset-password"element={<ResetPassword />}/>
         <Route path="/verify-email" element={<VerifyEmail />} />
       </Routes>
     </Router>
